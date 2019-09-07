beb0897d24e77df5a2db70776b7fc0b5d643968d
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-09-07 21:45:55 +0530

    init hash table

---
+++ hash table/src/student.java
@@ -0,0 +1,32 @@
+
+public class student {
+	private String name;
+	private int age;
+	private String school;
+	
+	public student(String n,int a,String s) {
+		this.name=n;
+		this.age=a;
+		this.school=s;
+	}
+	
+	public String  getName(String n) {
+		return name;
+	}
+	public void setName(String n) {
+		this.name=n;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int a) {
+		this.age=a;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String s) {
+		this.school=s;
+	}
+
+}
