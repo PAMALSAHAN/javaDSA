d215d4b3a654eb51cb5fff938db495d0d42ad96b
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2019-11-30 20:33:49 +0530

    init polymorphism

--- OOP/src/Polymorphism/methodOverLoading.java
+++ OOP/src/Polymorphism/methodOverLoading.java
@@ -1,5 +1,27 @@
 package Polymorphism;
 
-public class methodOverLoading {
+class Bike{
+	public void run() {
+		System.out.println("bike riding...");
+	}
+}
+public class methodOverLoading extends Bike {
+	
+	 public void run() {
+		System.out.println("this methd running ");
+	}
+	 public void sound() {
+		 System.out.println("in this method we are use sound method ");
+	 }
 
+	public static void main(String[] args) {
+		
+		methodOverLoading m=new methodOverLoading();
+		m.run();
+		m.sound();   //meke methodOverLOading kiyala dapu nisa thamai sound kiyana eka wada kranne.
+					// nathnam wada karanne naha.bike kiyana eka damma nam meke sound kiyana function eka ganne 
+					// naha.mokada eke variable ekata sound kiyana eka alla ganna baha.
+		
+		
+	}
 }
