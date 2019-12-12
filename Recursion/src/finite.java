80ee5ea8b426d27ae8ab12147a3829064d9b567d
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2019-12-12 21:48:13 +0530

    change count ++ but error occur

--- Recursion/src/finite.java
+++ Recursion/src/finite.java
@@ -1,14 +1,17 @@
-
+// fix the error
 public class finite {
 
 	static int count=0;
 	
 	static void p() {
-		count++;
+		
 		if(count<5) {
 			System.out.println("help"+count);
+			count++;
 			p();
+			
 		}
+		
 	}
 	
 	public static void main(String[] args) {
