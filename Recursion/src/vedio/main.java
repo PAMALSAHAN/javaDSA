2a8f0568d83e49c39d411f50a7bda2e49631e082
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-01-05 21:34:29 +0530

    init recursion code sample in the vedio

---
+++ Recursion/src/vedio/main.java
@@ -0,0 +1,17 @@
+package vedio;
+
+public class main {
+	public static void main(String[] args) {
+		printNumber(5);
+	}
+	
+	public static void printNumber(int num) {
+		if(num==0) {
+			
+		}
+		else {
+			System.out.println(num);
+			//printNumber(num-l);
+		}
+	}
+}
